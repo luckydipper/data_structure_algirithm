27945e3254fc6c2247263bb9532d3bea4d46ce08
Luckydipper <qhrqufdlek@naver.com>
2021-11-22 21:26:35 +0900

    [ADD] - pp09
    BST

--- data_structure_lecture/PP/PP09/ITreeType.py
+++ data_structure_lecture/PP/PP09/ITreeType.py
@@ -1,4 +1,7 @@
 
+from TreeType import TreeNode
+
+
 class NodeType:
     def __init__(self, data):
         self.info = data
@@ -11,29 +14,187 @@ class IterBST():
         self.order_list = []
 
     def insert(self, data):
-        '''[10]'''
+        node = TreeNode(data)
+        
+        if(self.root == None):
+            self.root = node
+            return
+        
+        iterator = self.root
+        while(True):
+            if(iterator.right == None and iterator.info < node.info):
+                iterator.right = node
+                break
+            elif(iterator.left == None and iterator.info >= node.info):
+                iterator.left = node
+                break
+            elif(iterator.info < node.info):
+                parent = iterator
+                iterator = iterator.right
+            else:
+                parent = iterator
+                iterator = iterator.left
+                
+            
+            
 
     def find(self, key):
-        '''[11]'''
+        self.find_node(self.root, key)
 
     def find_node(self, root, key):
-        '''[12]'''
+        
+        iterator = root
+        while(True):
+            if(iterator == None):
+                return False
+            elif(iterator.info == key):
+                return True
+            elif(iterator.info < key):
+                iterator = iterator.right
+            elif(iterator.info > key):
+                iterator = iterator.left
+            
 
     def delete(self, key):
-        '''[13]'''
+        self.delete_node(self.root, key)
 
     def delete_node(self, node, key):
-        '''[14]'''
-        
+        curr = node
+        prev = None
+    
+        # First check if the key is
+        # actually present in the BST.
+        # the variable prev points to the
+        # parent of the key to be deleted
+        while(curr != None and curr.info != key):
+            prev = curr
+            if curr.info < key:
+                curr = curr.right
+            else:
+                curr = curr.left
+    
+        if curr == None:
+            print("Key % d not found in\
+               the provided BST." % key)
+            return node
+    
+        # Check if the node to be
+        # deleted has atmost one child
+        if curr.left == None or\
+                curr.right == None:
+    
+            # newCurr will replace
+            # the node to be deleted.
+            newCurr = None
+    
+            # if the left child does not exist.
+            if curr.left == None:
+                newCurr = curr.right
+            else:
+                newCurr = curr.left
+    
+            # check if the node to
+            # be deleted is the node.
+            if prev == None:
+                return newCurr
+    
+            # Check if the node to be
+            # deleted is prev's left or
+            # right child and then
+            # replace this with newCurr
+            if curr == prev.left:
+                prev.left = newCurr
+            else:
+                prev.right = newCurr
+    
+            curr = None
+    
+        # node to be deleted
+        # has two children.
+        else:
+            p = None
+            temp = None
+    
+            # Compute the inorder
+            # successor of curr.
+            temp = curr.right
+            while(temp.left != None):
+                p = temp
+                temp = temp.left
+    
+            # check if the parent of the
+            # inorder successor is the node or not.
+            # if it isn't, then make the left
+            # child of its parent equal to the
+            # inorder successor's right child.
+            if p != None:
+                p.left = temp.right
+    
+            else:
+            
+                # if the inorder successor was
+                # the node, then make the right child
+                # of the node to be deleted equal
+                # to the right child of the inorder
+                # successor.
+                curr.right = temp.right
+    
+            curr.info = temp.info
+            temp = None
+    
+        return node
+
+
     def inorder(self, node):
-        '''[15]'''
+        current = self.root
+        stack = []
+        while(True):
+            if current is not None:
+                stack.append(current)
+                current = current.left
+            elif(len(stack) != 0):#element 있을 때 까지
+                current = stack.pop()
+                self.order_list.append(current.info)
+                current = current.right
+            else:
+                break
     
     def preorder(self, node):
-        '''[16]'''
+        current = self.root
+        stack = []
+        while(True):
+            if current is not None:
+                stack.append(current)
+                self.order_list.append(current.info)
+                current = current.left
+            elif(len(stack) != 0) :#element 있을 때 까지
+                current = stack.pop()
+                current = current.right
+            else:
+                break
     
     def postorder(self, node):
-        '''[17]'''
+        stack = []
+        root = self.root
+        while(True):
+            while(root != None):
+                stack.append(root)
+                stack.append(root)
+                root = root.left
+    
+            # Check for empty stack
+            if (len(stack) == 0):
+                return
+            
+            root = stack.pop()
+    
+            if (len(stack) > 0 and stack[-1] == root):
+                root = root.right
+            else:
+                self.order_list.append(root.info)
+                root = None
 
-    def get_predecessor(tree, data):
+
+    def     get_predecessor(tree, data):
         '''[18]'''
 
--- data_structure_lecture/PP/PP09/TreeType.py
+++ data_structure_lecture/PP/PP09/TreeType.py
@@ -15,33 +15,112 @@ class BST():
         return (self.root == None)
     
     def count_nodes(self, tree):
-        '''[1]'''
+        if(tree==None):
+            return 0
+        return self.count_nodes(tree.right) + self.count_nodes(tree.left) + 1
+        
     def length_is(self):
         return self.count_nodes(self.root)
 
     def insert(self, item):
-        '''[2]'''
+        find_node = TreeNode(item)
+        if(self.root == None):
+            self.root = find_node
+            return
+        self.insert_item(self.root, find_node)
 
-    def insert_item(self, node, item):
-        '''[3]'''
+    # node return
+    def insert_item(self, node, item):# call by reference가 안 돼서... ? 
+        if(node.right == None  and  item.info >= node.info):
+            node.right = item
+        elif(node.left == None  and item.info < node.info):
+            node.left = item
+        elif(node.info <= item.info):
+            self.insert_item(node.right, item)
+        elif(node.info > item.info):
+            self.insert_item(node.left, item)
         
-  
+        
+        
+            
+        
+
     def inorder(self, node):
-        '''[4]'''
+        if(node == None):
+            return 
+        self.inorder(node.left)
+        self.order_list.append(node.info)
+        self.inorder(node.right)
     
     def preorder(self, node):
-        '''[5]'''
+        if(node == None):
+            return 
+        self.order_list.append(node.info)
+        self.preorder(node.left)
+        self.preorder(node.right)
+
     
     def postorder(self, node):
-        '''[6]'''
+        if(node == None):
+            return 
+        self.postorder(node.left)
+        self.postorder(node.right)
+        self.order_list.append(node.info)
 
     def delete(self, item):
-        '''[7]'''
+        self.delete_node(self.root, item)
     
     def delete_node(self, current, item):
-        '''[8]'''
+        # pointer도 없고, 함수에서 받은 parameter 바꾸면, call by reference도 안 된다.
+        # python에서는 새로운 객체를 만드는 것이 파이썬스러울 것 같다... 
+        # parent node까지 찾아서 구현하는 것 보다는 전체를 복사하는게 이득일 수 있다.
+        self.preorder(current)
+        
+        deleted_bst = BST()
+        for node_info in self.order_list:
+            if(node_info != item):
+                deleted_bst.insert(node_info)
+        
+        self.root = deleted_bst.root
+        self.order_list.clear()
+        
+        # if current is None:
+        #     return current
+        
+        # if item < current.info:
+        #     current.right = self.delete_node(current.left, item)
+            
+        # elif(item > current.info):
+        #     current.right = self.delete_node(current.right, item)
+        
+        # else:
+        #     if current.left is None:
+        #         temp = current.right
+        #         root = None
+        #         return temp
 
-    def get_predecessor(tree, data):
-        '''[9]'''
+        #     elif current.right is None:
+        #         temp = current.left
+        #         root = None
+        #         return temp
+        
+        #     temp = self.get_predecessor(current.right)
+        #     # Copy the inorder successor's
+        #     # content to this node
+        #     current.key = temp.key
+
+        #    # Delete the inorder successor
+        #     current.right = self.delete_node(current.right, temp.key)
+        
+            
+    #이 함수를 써서 delete node를 찾는다 한들, parent node까지 찾아야 한다.
+    def get_predecessor(self, tree, data):
+        current = tree
+ 
+    # loop down to find the leftmost leaf
+        while(current.left is not None):
+            current = current.left
+ 
+        return current
 
         
