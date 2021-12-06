0a62d97cbcd618d1b33d47cf056780d07ea3f55b
Luckydipper <qhrqufdlek@naver.com>
2021-12-06 23:39:18 +0900

    [ADD] - lab13 exercise1

---
+++ data_structure_lecture/data_structure/data_structure/SelectionSort.cpp
@@ -0,0 +1,29 @@
+#include "Student.h"
+#include <string>
+bool operator> (Student l, Student r)
+{
+	std::string l_string = l.getName();
+	std::string r_string = r.getName();
+	if (l_string > r_string)
+		return true;
+	else
+		return false;
+}
+
+int MinIndex(Student values[], int startIndex, int endIndex)
+{
+	int indexOfMin = startIndex;
+	for (int index = startIndex + 1; index <= endIndex; index++)
+	{
+		if (values[indexOfMin] > values[index]) // 왼쪽이 크면 1, 오른쪽이 크면 -1 
+			indexOfMin = index;
+	}
+	return indexOfMin;
+}
+
+void SelectionSort(Student values[], int numValues)
+{
+	int endIndex = numValues - 1;
+	for (int current = 0; current < endIndex; current++)
+		Swap(values[current], values[MinIndex(values, current, endIndex)]);
+}
---
+++ data_structure_lecture/data_structure/data_structure/SelectionSort.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "Student.h"
+int MinIndex(Student values[], int startIndex, int endIndex);
+void SelectionSort(Student values[], int numValues);
