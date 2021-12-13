87f3e7f2b6501db8ce44983fc2449f8a207e44a0
Luckydipper <qhrqufdlek@naver.com>
2021-12-13 19:24:58 +0900

    [ADD] - Bubble Sort

--- data_structure_lecture/PP/pp11/BubbleSort.py
+++ data_structure_lecture/PP/pp11/BubbleSort.py
@@ -1,6 +1,14 @@
 
 def bubble_sort(values):
-    """[1]"""
+    
+    for sorted_boundery_index, _  in enumerate(values):
+        smallest_index = len(values) -1
+        
+        for reverse_index in range(len(values) -1, sorted_boundery_index ,-1):
+            if(values[reverse_index-1] > values[reverse_index]):
+                temp = values[reverse_index-1]
+                values[reverse_index-1] = values[reverse_index]
+                values[reverse_index] = temp
 
 
 
