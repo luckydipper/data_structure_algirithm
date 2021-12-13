ddd4e9426a514daea5813d2005f89becb1922226
Luckydipper <qhrqufdlek@naver.com>
2021-12-13 19:45:41 +0900

    [ADD] - insert sort

--- data_structure_lecture/PP/pp11/InsertionSort.py
+++ data_structure_lecture/PP/pp11/InsertionSort.py
@@ -1,4 +1,8 @@
 def insertion_sort(values):
-    
-    '''[4]'''
-    
+    for sorted_boundary_index, _ in enumerate(values):
+        for searching_index in range(sorted_boundary_index+1):
+            if values[searching_index] >= values[sorted_boundary_index]:
+                temp = values[sorted_boundary_index]
+                del values[sorted_boundary_index]
+                values.insert(searching_index, temp)
+            
