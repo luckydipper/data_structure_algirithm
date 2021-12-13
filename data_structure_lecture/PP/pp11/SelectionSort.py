3bfbd679437610ce50bc0c87837ec064739a47a8
Luckydipper <qhrqufdlek@naver.com>
2021-12-13 19:33:57 +0900

    [ADD] - selection sort

--- data_structure_lecture/PP/pp11/SelectionSort.py
+++ data_structure_lecture/PP/pp11/SelectionSort.py
@@ -1,4 +1,17 @@
 
 def selection_sort(values):
+    for sorted_boundery_index, _  in enumerate(values):
+        smallest_index = sorted_boundery_index
+        
+        # searching smallest index
+        for searching_index in range(sorted_boundery_index, len(values)):
+            if values[smallest_index] > values[searching_index]:
+                smallest_index = searching_index
+    
+        # switch sorted_boundery_index to smallest index
+        temp = values[sorted_boundery_index]
+        values[sorted_boundery_index] = values[smallest_index]
+        values[smallest_index] = temp
+            
 
-    '''[9]'''
+    
