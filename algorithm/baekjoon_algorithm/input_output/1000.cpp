9cc41970442bbd6db5f366f426531783f8615dd1
luckydipper <qhrqufdlek@naver.com>
2022-04-06 13:47:24 +0000

    [ADD] - io algorithm

---
+++ algorithm/baekjoon_algorithm/input_output/1000.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int first, second;
+	cin >> first >> second;
+	cout << first + second;
+}
---
+++ algorithm/baekjoon_algorithm/input_output/2557.cpp
@@ -0,0 +1,6 @@
+#include <cstdio>
+
+int main()
+{	
+	std::printf("%s","Hello World!");
+}
