51c1d82423bc282bd6d691dd4814218734f1f7b8
luckydipper <qhrqufdlek@naver.com>
2022-11-11 13:12:34 +0000

    [ADD] - bottom up and top down code

--- baekjoon_algorithm/basic_lecture1/dynamic_programing/1463.cpp
+++ baekjoon_algorithm/basic_lecture1/dynamic_programing/1463.cpp
@@ -11,18 +11,71 @@ using namespace std;
 // D[N] : N을 1까지 만드는데 걸리는 최소 횟수 
 // D[N] = min(D[N/3], D[N/2], D[N-1])
 
-int count_call(int N)
+
+int count_call(int N, vector<int>& cache)
 {
-    while(cache[N] == 0)
+    if(cache[N] != 0)
     {
-        if(N % 3 == 0)
-            cache[N] = cache[N/3] + 1;
-        else if(N % 2 == 0)
-            cache[N] = cache[N/2] + 1;
-        else 
-            cache[N] = cache[N-1] + 1;
+        return cache[N];
+    }
+    if(N <= 1)
+    {
+        return 0;
+    }
+    else
+    {
+    int out = count_call(N-1, cache) + 1;
+    
+    if(N % 3 == 0) 
+    {
+        int devided_3 = count_call(N/3, cache) + 1;
+        if(devided_3 < out) out = devided_3;
+    }
+
+    if(N % 2 == 0)
+    {
+        int devided_2 = count_call(N/2, cache) + 1;
+        if(devided_2 < out) out = devided_2;
+    }
+
+    cache[N] = out;
+    return out;
     }
 }
+
+int bottom_up_to_1(int n, vector<int> & cache)
+{
+    cache[1] = 0;
+    for(int i = 2; i <= n; i++)
+    {
+        cache[i] = cache[i-1] + 1;
+        
+        if( i % 3 == 0  && cache[i/3] < cache[i])
+            cache[i] = cache[i/3] + 1;
+        
+        if( i % 2 ==0 && cache[i/2] < cache[i])
+            cache[i] = cache[i/2] + 1;
+    }
+    return cache[n];
+}
+
+// int go(int n)
+// {
+//     if( n == 1 ) return 0;
+//     if( d[n] > 0) return d[n];
+//     d[n] = go(n-1) + 1;
+//     if(n%2 == 0)
+//     {
+//         int temp = go(n/2) + 1;
+//         if(d[n] > temp) d[n] = temp;
+//     }
+//     if(n%3 == 0)
+//     {
+//         int temp = go(n/3) + 1;
+//         if(d[n] > temp) d[n] = temp;
+//     }
+//     return d[n];
+// }
 
 int main()
 {
@@ -34,11 +87,6 @@ int main()
 
     vector<int> cache(N+1, 0);
     
-    cache[1] = 0;
-    cache[2] = 1;
-    cache[3] = 1;
-
-
-
-    return cache[N]
+    cout << count_call(N, cache) << '\n';
+    return 0;
 }
