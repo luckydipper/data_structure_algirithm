e388a2b6345f1064178b4b00c2226cd857263912
luckydipper <qhrqufdlek@naver.com>
2022-11-26 04:20:43 +0000

    [ADD] - unordered_map data structure
    If we use AVL tree, Input part time complexity can be O(N logN).

--- baekjoon_algorithm/step_by_step/set_map/10816.cpp
+++ baekjoon_algorithm/step_by_step/set_map/10816.cpp
@@ -1,29 +1,37 @@
 #include <iostream>
 #include <set>
+#include <unordered_map> // hash table based map
 using namespace std;
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+    
     int N;
     cin >> N;
 
-    multiset<int> having_cards;
+    unordered_map<int, int> having_cards;
 
     for(int i = 0; i < N; i++)
     {
-        int temp;
-        cin >> temp;
-        having_cards.insert(temp);
+        int card_number;
+        cin >> card_number;
+
+        bool success_insertion = having_cards.insert(pair<int,int>(card_number, 1)).second;
+        if(!success_insertion)
+            having_cards[card_number]++;
     }
 
     int M;
     cin >> M;
     for(int i = 0; i < M; i++)
     {
-        int temp;
-        cin >> temp;
-        cout << having_cards.count(temp) << ' ';
+        int card_number;
+        cin >> card_number;
+        if(having_cards.find(card_number) == having_cards.end())
+            cout << "0 ";
+        else
+            cout << having_cards.find(card_number)->second << ' ';
     }
 }
