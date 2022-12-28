54d82510a94f5a279edcc84d0ee15e1f53e9475b
luckydipper <qhrqufdlek@naver.com>
2022-12-28 09:59:55 +0000

    [TODO] - topology sorting with debug mode and DFS

--- baekjoon_algorithm/topology_sort/2252.cpp
+++ baekjoon_algorithm/topology_sort/2252.cpp
@@ -4,59 +4,49 @@
 #include <algorithm>
 using namespace std;
 
-//void makeGraph()
-//adj list 쓰기로 결정 matrix는 크기가 너무 큼.
+//adj list 쓰기로 결정 matrix는 크기가 너무 큰 듯?
 
-
+// 이건 declare만 한 것이라 전역변수 취급이 안되나?
 int N, M;
 vector<list<int>> adj;
+vector<bool> visited;
+vector<int> order;
 
-vector<int> seen;
-vector<int> result;
-
+// dfs' input nodes are not visited 
 void dfs(int here){
-    if(seen[here] == 1)
-        return ;
-    else{
-        seen[here] = 1;
-        list<int>::iterator i = adj[here].begin();
-        while( i != adj[here].end()){
+    visited[here] = true; // 이 부분에서 에러가 발생 
+    for(list<int>::iterator i = adj[here].begin(); i != adj[here].end(); i++)
+    {
+        cout <<"*i : " << *i<< "\n";
+        if(visited[*i] == false)
             dfs(*i);
-            i++;
-        }
     }
-    result.push_back(here);
+    order.push_back(here);
+}
+
+void make_graph() // 이 함수를 main 안에 집어넣어서 동작시키면 잘 동작함.
+{
+    while(M--){
+        int from, to;
+        cin >> from >> to;
+        adj[from].push_back(to);
+    }
 }
 
 int main()
 {
     cin >> N >> M;
-    //list는 초기화 하지 않아도 되는가?
     vector<list<int>> adj(N+1);
-    vector<int> find_root_node(N+1, 1);
+
+    make_graph();
     
-    while(M--)
-    {
-        int tall, small;
-        cin >> tall >> small;
-        adj[tall].push_back(small);
-        find_root_node[small] = 0;
-    }
+    vector<bool> visited(N+1, false);
+    for(int i = 1; i < N+1; i++)
+        dfs(i);
 
-    // find indegree zero vertex
-
-    int root_index = -1;
-    for(int i = 1; i < find_root_node.size(); i++){
-        if(find_root_node[i] == 1){
-            root_index = i;
-        }
-    }
-
-    //dfs
-    vector<int> seen(N+1, 0);
-    dfs(root_index);
-    reverse(result.begin(), result.end());
-
-    for(int i : result)
-        cout << i << " ";
+ 
+    reverse(order.begin(), order.end());
+    
+    for(int elem: order)
+        cout << elem << " ";
 }
