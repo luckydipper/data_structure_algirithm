030c9a791213bb39b2b0ac7b97c0aedc619e504d
luckydipper <qhrqufdlek@naver.com>
2022-12-28 11:36:05 +0000

    [COMPLETE] - if command's order is important!

--- baekjoon_algorithm/step_by_step/recursive/2447.cpp
+++ baekjoon_algorithm/step_by_step/recursive/2447.cpp
@@ -2,7 +2,14 @@
 // 문제를 나눠서
 using namespace std;
 
-
+void star(int i, int j, int n){
+    if((i/n)%3 == 1 && (j/n)%3 == 1)
+        cout << " ";
+    else if(n == 1)
+        cout << "*"; // n == 1을 맨 앞으로 빼면, n 이 1일때 무조건 star가 찍혀서 망함
+    else
+        star(i, j, n/3);
+}
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -11,14 +18,10 @@ int main(){
 
     int N;
     cin >> N;
+    // i,j좌표에 뭐가 들어가는지 찾아서 출력
     for(int i = 0; i < N; i++){
         for(int j=0;j < N; j++){
-            for(int n = 1; n <= N/3; n*=3){
-                if((i/(n))%3 == 1 && (j/(n))%3 == 1)
-                    cout << " ";
-                else
-                    cout << "*";
-            }
+            star(i, j, N);
         }
         cout << "\n";
     }
