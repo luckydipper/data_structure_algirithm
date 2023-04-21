3fedbaeead35a850a5ee5ed9faa8f35a8d8dd11a
luckydipper <qhrqufdlek@naver.com>
2023-04-21 13:17:46 +0000

    [ADD] - hanoi problem

--- baekjoon_algorithm/hellow_world.cpp
+++ baekjoon_algorithm/hellow_world.cpp
@@ -1,7 +1,16 @@
-#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void hanoi(int n, string from, string to, string by){
+	if(n == 1){
+		cout << "move " << from << " -> " << to <<" \n";
+		return;
+	}hanoi(n-1,from,by,to);
+	hanoi(1,from,to,by);
+	hanoi(n-1,by, to, from);
+}
 
 int main(int argc, char** argv)
 {
-	std::cout << "hello world!\n";
-	std::cout << "hello military life" << std::endl;
+	hanoi(3,"A","B","C");
 }
