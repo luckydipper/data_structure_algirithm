1f56718173170873e6ad436659744f387d654496
luckydipper <qhrqufdlek@naver.com>
2023-04-26 11:17:29 +0000

    [ADD] - back tracking solution

--- baekjoon_algorithm/back_tracking/14888.cpp
+++ baekjoon_algorithm/back_tracking/14888.cpp
@@ -18,36 +18,50 @@ int do_calculate(int l, int r, int op){
         return -1;
 }
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cin >> N;
-    for(int i = 0; i < N; i++)
-        cin >> arr[i];
-    
-    vector<int> spaned_operations;
-    
-    for(int i = 0; i < 4; i++){
-        int tmp;
-        cin >> tmp;
-        while(tmp--)
-            spaned_operations.push_back(i);
-    }
 
-    int min_result = 0x7F7F7F7F;
-    int max_result = -0x7F7F7F7F; //1e-9 는 소수 자리수임. -1000
-    do{
+//back_tracking solution
+vector<int> picked;
+int min_result = 0x7F7F7F7F;
+int max_result = -0x7F7F7F7F; //1e-9 는 소수 자리수임. -1000
+
+int num_op[4];
+
+void operationPermutation(int to_pick){
+    if(picked.size() == to_pick){
         int result = arr[0];
-        for(int i = 0; i < spaned_operations.size(); i++){
-            int op = spaned_operations[i];
+        for(int i = 0; i < picked.size(); i++){
+            int op = picked[i];
             result = do_calculate(result, arr[i+1], op);
         }
         if(result < min_result )
             min_result = result;
         if(result > max_result )
             max_result = result;
+        return;
+    }
+    for(int i = 0; i < 4; i++){
+        if(num_op[i] == 0) continue;
+        num_op[i]--;
+        picked.push_back(i);
+        operationPermutation(to_pick);
+        num_op[i]++;
+        picked.pop_back();
+    }
+}
 
-    }while(next_permutation(spaned_operations.begin(), spaned_operations.end()));
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> N;
+    for(int i = 0; i < N; i++)
+        cin >> arr[i];
+
+    for(int i = 0; i < 4; i++)
+        cin >> num_op[i];
+    
+    operationPermutation(N-1);
 
     cout << max_result << "\n" << min_result;
 }
