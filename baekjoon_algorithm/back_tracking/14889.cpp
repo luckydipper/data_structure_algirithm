bc303f7ae309d6f982f02485d7af8996d7275293
luckydipper <qhrqufdlek@naver.com>
2023-04-30 03:44:57 +0000

    [ADD] - combination with two picked_array
    smallest index를  parameter로 넘김.

--- baekjoon_algorithm/back_tracking/14889.cpp
+++ baekjoon_algorithm/back_tracking/14889.cpp
@@ -3,34 +3,49 @@ using namespace std;
 
 int N;
 int ability_map[21][21];
-vector<int> ability_arr;
 int result_min = 987654321;
-//permutation으로 구현 해보자.
 
-bool is_picked[21];
-vector<int> picked_index;
-vector<int> sum_partial;
 
-//combination 이네
-void update_sum_partial(int n, int m){
-    if(picked_index.size() == m){
-        int result = 0;
-        for(int idx: picked_index){
-            cout << idx << " ";
-            result += ability_arr[idx];
+vector<int> start_picked;
+
+
+void halfCombination(int len_ability_array){
+    if(start_picked.size() == len_ability_array/2)
+    {
+        int start_sum = 0, link_sum = 0;
+
+        vector<bool> is_index_link_picked;
+        for(int i = 0; i < len_ability_array; i++)
+            is_index_link_picked.push_back(true);
+
+        for(int idx_x: start_picked){
+            is_index_link_picked[idx_x] = false;
+            for(int idx_y: start_picked)
+                start_sum += ability_map[idx_x][idx_y];
         }
-        cout << "\n";
-        sum_partial.push_back(result);
+        vector<int> linked_picked_index;
+        for(int i = 0; i < len_ability_array; i++)
+            if(is_index_link_picked[i])
+                linked_picked_index.push_back(i);
+        
+        for(int idx_x: linked_picked_index){
+            for(int idx_y: linked_picked_index)
+                link_sum += ability_map[idx_x][idx_y];
+        }
+
+        if(result_min > abs(start_sum- link_sum))
+            result_min = abs(start_sum-link_sum);
         return;
     }
-    int smallest = picked_index.empty() ? 1 : picked_index.back()+ 1;
+    int smallest = start_picked.empty()? 0 : start_picked.back()+1;
 
-    for(int i = smallest; i < n; i++){
-        picked_index.push_back(i);
-        update_sum_partial(n, m);
-        picked_index.pop_back();
+    for(int i = smallest; i< len_ability_array; i++){
+        start_picked.push_back(i);
+        halfCombination(len_ability_array);
+        start_picked.pop_back();
     }
 }
+
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -41,20 +56,9 @@ int main(){
     for(int i = 0; i < N; i++)
         for(int j = 0; j < N; j++)
             cin >> ability_map[i][j];
-
-    for(int i = 0; i < N; i++)
-        for(int j = i+1; j < N; j++)
-            ability_arr.push_back(ability_map[i][j] + ability_map[j][i]);
-
-    sort(ability_arr.begin(), ability_arr.end());
     
-    for(int i = 0; i < ability_arr.size(); i++){
-        update_sum_partial(ability_arr.size(), i);
-    }
+    halfCombination(N);
 
-    for(int i : sum_partial)
-        cout << i << " ";
-    cout << "\n";
-    sort(sum_partial.begin(), sum_partial.end());
+    cout << result_min;
 
 }
