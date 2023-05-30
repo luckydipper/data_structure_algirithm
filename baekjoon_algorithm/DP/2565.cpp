44507786d701fe0c6a72117a5cacb1ad8abb54e4
luckydipper <qhrqufdlek@naver.com>
2023-05-30 14:18:33 +0000

    [ADD] - sort + lis solution

--- baekjoon_algorithm/DP/2565.cpp
+++ baekjoon_algorithm/DP/2565.cpp
@@ -2,62 +2,22 @@
 using namespace std;
 
 int N;
-int from_to[101][2];
-int cache[101][2]; // 1~i 번째까지, 뺀 것 or 안 뺀 것;
-int cache2[101];
-const int INF = 2147483647;
+pair<int,int> from_to[101];
+int cache[101]; // 1~i 번째까지, 뺀 것 or 안 뺀 것;
 
-bool isCrossed(int line1,int line2){
-    return (from_to[line1][0] - from_to[line2][0]) * (from_to[line1][1] - from_to[line2][1]) < 0;
+bool is_acrossed(int here,int there){//from_to[there].first > from_to[here].first 자명 
+    return from_to[there].second < from_to[here].second;
 }
-
-// int minExtractLine(int here, int is_extracted){ //1~here, 
-//     if(is_extracted){ return 0;}
-//     if(here == 1) return 1;
-    
-//     int& ret = cache[here][is_extracted];
-//     if(ret != -1) return ret;
-
-//     int num_crossed = 0;
-//     for(int i = 1; i <= here; i++){
-//         if(isCrossed(i,here)){// cache2를 infiity 로 보냈다가도 돌려 놔야 해.
-//             //is_extracted[i] = 1; //back tracking마냥 inf 를 돌려놔야 하는데
-//             cache[i][is_extracted] = 1;
-//             num_crossed++;
-//         }
-//     }
-
-//     if(num_crossed == 0) 
-//         return ret = minExtractLine(here-1);
-//     else                // here를 빼버림             here과 겹치는 것들을 빼버림
-//         return ret = min( minExtractLine(here-1) + 1, minExtractLine(here-1) + num_crossed ); 
-//     //fill(is_extracted,is_extracted+101, 0);
-// }
-
-
-// 1 ~ here까지 고려할 때, 겹치지 않기 위한 최소 전선 갯수.
-//bool is_extracted[101];
-
-int minExtractLine(int here){ //1~here, 
-    //if(extraced_indexs.find(here)){ is_extracted[here] = 1; return 0;}
-    if(here == 1) return 1;
-    
-    int& ret = cache2[here];
+int LIS(int here){
+    int &ret = cache[here];
     if(ret != -1) return ret;
 
-    int num_crossed = 0;
-    for(int i = 1; i <= here; i++){
-        if(isCrossed(i,here)){// cache2를 infiity 로 보냈다가도 돌려 놔야 해.
-            //is_extracted[i] = 1; //back tracking마냥 inf 를 돌려놔야 하는데
-            num_crossed++;
-        }
+    ret = 1;
+    for(int there = here+1; there <= N; there++){
+        if(is_acrossed(here, there)) continue;
+        ret = max(ret, LIS(there)+1);
     }
-
-    if(num_crossed == 0) 
-        return ret = minExtractLine(here-1);
-    else                // here를 빼버림             here과 겹치는 것들을 빼버림
-        return ret = min( minExtractLine(here-1) + 1, minExtractLine(here-1) + num_crossed ); 
-    //fill(is_extracted,is_extracted+101, 0);
+    return ret;
 }
 
 int main(){
@@ -65,9 +25,11 @@ int main(){
     cin.tie(nullptr);
     cin >> N;
     for(int i = 1; i <= N; i++)
-        cin >> from_to[i][0] >> from_to[i][1];
-    //sort(from_to+1, from_to)
-    memset(cache2,-1,sizeof(cache2));
-    //cout << min(minExtractLine(1,0), minExtractLine(1,1));
-    cout << minExtractLine(N);
+        cin >> from_to[i].first >> from_to[i].second;
+    memset(cache, -1, sizeof(cache));
+    sort(from_to+1,from_to+N+1);
+    //for(int i = 1; i <= N; i++)
+    //    cout << from_to[i].first << " " << from_to[i].second << "\n";
+    //from_to[0].first = from_to[0].second = -2147483647;
+    cout << N + 1 - LIS(0);
 }
