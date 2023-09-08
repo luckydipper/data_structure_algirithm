05278fe51250ae11c2b48023074107fdec318a36
luckydipper <qhrqufdlek@naver.com>
2023-09-08 14:01:35 +0900

    [ADD] - 1103 complete DP + DFS + backtracking

--- baekjoon_algorithm/DP/1103.cpp
+++ baekjoon_algorithm/DP/1103.cpp
@@ -1,4 +1,12 @@
 #include <bits/stdc++.h>
+// 처음에 갈 수 있는 곳이 두곳일 때. 다른 길 하나는 일방 통행일 경우를 고려해야함. 
+// ex)
+//5 5
+//4HHH9
+//HHHHH
+//HHH12
+//HHHHH
+//3HH2H
 // is_visited와 cache는 다르게 설정 하자. 방문한 곳 또 방문 한것과, 해당 지점에서 최대한으로 반복할 수 있는 횟수는 따로 저장해야 한다. 
 
 using namespace std;
@@ -6,37 +14,48 @@ using namespace std;
 int size_y, size_x;
 int momentum_map[51][51];
 int cache[51][51];
+bool is_visited[51][51]; 
 int const dy[] = {1, -1,  0, 0};
 int const dx[] = {0,  0, -1, 1};
 
 const int INF = 210000000;
 bool isSafe(int y, int x){
     //cout << "y :" << y << " x : " << x << " ";
+    //cout << " y: "  << y << " x : " << x << "\n" << momentum_map[y][x] << " " << ('H'-'0') << "\n"; 
     return  1 <= y && y <= size_y && 1 <= x && x <= size_x && momentum_map[y][x] != ('H'-'0');
 }
 
 int countMaxMovable(int y, int x){
     if(!isSafe(y, x))
         return 0;
-    //cout << "is safe! \n";
+    //cout << y << ", " << x <<  " is safe! \n";
 
-    int &visit_sequence = cache[y][x];
-    // 무한 루프이다. 왔던 곳이다.
-    if(visit_sequence != -1 && visit_sequence < INF){ 
-        cout << -1;  
+    if(is_visited[y][x]){
+        //cout << y << ", " << x << "\n";
+        cout << -1;
         exit(0);
     }
-    
-    int new_y = y + dy[0] * momentum_map[y][x];
-    int new_x = x + dx[0] * momentum_map[y][x];
-    int current_max_mvabl = countMaxMovable(new_y, new_x);
 
-    for(int direction = 1; direction < 4; direction++){
-        new_y = y + dy[direction] * momentum_map[y][x];
-        new_x = x + dx[direction] * momentum_map[y][x];
-        current_max_mvabl = max(current_max_mvabl, countMaxMovable(new_y, new_x));
+    ; //4방위를 다 검사 한 후 visit 도장 찍어줘
+  
+    int &visit_sequence = cache[y][x];
+    // 무한 루프이다. 왔던 곳이다.
+    if(visit_sequence != -1)
+        return visit_sequence;
+    
+
+    int current_max_mvabl = 0;
+
+    is_visited[y][x] = true; //dfs back tracking 와 같이 구현 
+    for(int direction = 0; direction < 4; direction++){
+        int new_y = y + dy[direction] * momentum_map[y][x];
+        int new_x = x + dx[direction] * momentum_map[y][x];
+        //cout << y << ", " << x << " call -> " << new_y << ", " << new_x << "\n";
+        current_max_mvabl = max(current_max_mvabl, countMaxMovable(new_y, new_x) + 1);
     }
-    return visit_sequence = current_max_mvabl + 1;
+    is_visited[y][x] = false;
+
+    return visit_sequence = current_max_mvabl;
 }
 
 
@@ -53,12 +72,8 @@ int main(){
         for(int j = 1; j <= size_x; j++)
             momentum_map[i][j] = tmp[j-1]-'0';
     }
+    
 
-    int result = countMaxMovable(1, 1);
-
-    if(result >= INF)
-        cout << -1;
-    else
-        cout << result;
+    cout << countMaxMovable(1, 1);
     return 0;
 }
